063a6794f49b60e9103d3a1f011820c5f053d935
rajatchaurasia123 <rajat.chaurasia@walkingtree.tech>
2022-04-11 14:45:00 +0530

    hello

--- src/main/java/com/example/demo/Interface/Employeerepo.java
+++ src/main/java/com/example/demo/Interface/Employeerepo.java
@@ -3,10 +3,10 @@ package com.example.demo.Interface;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Employee;
-@Repository
+@Service
 public interface Employeerepo extends JpaRepository<Employee, Integer> {
 
 	Optional<Employee> findById(int empId);
--- src/main/java/com/example/demo/entity/Employee.java
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -1,11 +1,11 @@
 package com.example.demo.entity;
 
 import javax.persistence.Entity;
+
 import javax.persistence.Id;
 
 
 	@Entity
-	
 	public class Employee {
 		@Id
 		int EmpId;
