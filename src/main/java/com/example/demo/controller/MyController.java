b1225851596cfd0d1fb45f4a33e3fcbad2a224cc
rajatchaurasia123 <rajat.chaurasia@walkingtree.tech>
2022-04-11 13:20:09 +0530

    hello

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Interface.Employeerepo;
 import com.example.demo.entity.Employee;
+@RestController
 public class MyController {
 	
 	@Autowired
